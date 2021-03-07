6817f665d22111b77b2d0c9da851c0b377d31257
Александр Михайлов <thekulc@gmail.com>
2021-03-08 01:01:11 +0400

    feat: add chainMaker implement

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  cArr: [],
+  finished: { is: false },
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.cArr.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.finished.is) {
+      this.cArr = []
+      this.finished.is = false
+    }
+    if (value === null) {
+      this.cArr.push("null")
+      return this
+    }
+    if (typeof(value)==="boolean"){
+      this.cArr.push(value ? "true" : "false");
+      return this
+    }
+    if (typeof(value) === "number" && isNaN(value)){
+      this.cArr.push("NaN");
+      return this
+    }
+    if (value || value === 0)
+      this.cArr.push(value.toString())
+    else
+      this.cArr.push("")
+    return this
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!position || !this.cArr.hasOwnProperty(position - 1)) {
+      this.finished.is = true
+      throw new Error("")
+    }
+    this.cArr.splice(position - 1, 1);
+    return this
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.cArr.reverse();
+    return this
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.finished.is = true;
+    return this.cArr.map(el => `( ${el} )`).join("~~");
   }
 };
 
