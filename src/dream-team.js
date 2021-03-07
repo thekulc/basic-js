d20f73bf1fd3b006cd434bdafa5c6a41129479e5
Александр Михайлов <thekulc@gmail.com>
2021-03-07 20:37:22 +0400

    feat: add dream team implement

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  return cd(members)
 };
+
+function cd(members) {
+  try {
+    if (!members || !Array.isArray(members) || members.length <= 0) throw new Error()
+    const res = members.map(m => {
+      if (typeof (m) !== "string" || !m.trim()) return "";
+      return m.trim()[0];
+    }).sort((a, b) => a.localeCompare(b)).join("").toUpperCase();
+    return res ? res : false
+  } catch (error) {
+    return false
+  }
+}
