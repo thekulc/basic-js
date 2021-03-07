d6d47fadf84f70cc9b22a9ccd8e7ebc8f3843f5c
Александр Михайлов <thekulc@gmail.com>
2021-03-07 18:44:00 +0400

    feat: add countCats implement

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  return cc(matrix)
 };
+
+function cc(matrix) {
+  let cats = 0;
+  matrix.forEach(c => c.forEach(el => { if (el === "^^") cats++ }))
+  return cats
+}
